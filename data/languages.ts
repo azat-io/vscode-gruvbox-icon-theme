b0a8e10a9ae1d40d64d21c48b1690f6c3ce04fd5
Azat S <to@azat.io>
2023-02-06 01:55:01 +0300

    feat: add css icon

--- data/languages.ts
+++ data/languages.ts
@@ -2,6 +2,11 @@ import type { LanguageIcon } from '../typings/index.d.js'
 
 export let languagesIcons: LanguageIcon[] = [
   {
+    id: 'css',
+    name: 'CSS',
+    extensions: ['css'],
+  },
+  {
     id: 'html',
     name: 'HTML',
     extensions: ['htm', 'html'],
