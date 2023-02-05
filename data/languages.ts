fa94272d25efd5de914a49a5a2d2a65cd6fa7999
Azat S <to@azat.io>
2023-02-06 02:18:25 +0300

    feat: add javascript icon

--- data/languages.ts
+++ data/languages.ts
@@ -2,6 +2,11 @@ import type { LanguageIcon } from '../typings/index.d.js'
 
 export let languagesIcons: LanguageIcon[] = [
   {
+    id: 'javascript',
+    name: 'JavaScript',
+    extensions: ['javascript', 'js', 'mjs'],
+  },
+  {
     id: 'css',
     name: 'CSS',
     extensions: ['css'],
