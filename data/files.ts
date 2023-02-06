d4d63b3235b1787d46faa283e15edbb0110c4fdf
Azat S <to@azat.io>
2023-02-07 00:20:11 +0300

    feat: add eslint icon

--- data/files.ts
+++ data/files.ts
@@ -2,6 +2,22 @@ import type { FileIcon } from '../typings/index.d.js'
 
 export let filesIcons: FileIcon[] = [
   {
+    id: 'eslint',
+    name: 'ESLint',
+    files: [
+      '.eslintrc.js',
+      '.eslintrc.cjs',
+      '.eslintrc.yaml',
+      '.eslintrc.json',
+      '.eslintrc-md.js',
+      '.eslintrc-jsdoc.js',
+      '.eslintrc',
+      '.eslintignore',
+      '.eslintcache',
+      '.eslint.config.js',
+    ],
+  },
+  {
     id: 'nodejs',
     name: 'NodeJS',
     files: [
